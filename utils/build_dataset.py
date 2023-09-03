f97eac5057879d9102a0db264f5dfd2033357a41
Rahul Kumar <rahulkm@berkeley.edu>
2023-09-03 15:24:21 -0700

    dataset builder clean

--- utils/build_dataset.py
+++ utils/build_dataset.py
@@ -1,61 +1,70 @@
-import os, csv
+import os
+import csv
 from detectron2.structures import BoxMode
 
+
 def build_dataset(dataDir, mode):
-	# label directory that has csv for all tifs
-	labelsDir = os.path.join(dataDir, "labels(xywh)")
-	dataset_dicts = []
-	# id for the image we are iterating on
-	imgID = 1
+    """
+    Construct a dataset for Detectron2.
 
-	# image directory
-	imgDir = os.path.join(dataDir, mode)
-	print('Loading images from {} for {}'.format(imgDir, mode))
+    Parameters:
+    - dataDir (str): The root directory containing the dataset.
+    - mode (str): The type of dataset, e.g., "train", "val", "test".
 
-	# iterating through each image (filename) in sorted order
-	for image in sorted(os.listdir(imgDir)):
-		if image.endswith(".tif"):
-			# a dictionary to store all of the characteristics of the image
-			record = {}
-			record['file_name'] = os.path.join(imgDir, image)
-			record['image_id'] = imgID
-			record['height'] = 1024
-			record['width'] = 1024
-			imgID += 1
+    Returns:
+    - List[Dict]: A list of dictionaries where each dictionary represents an image and its annotations.
+    """
 
-			# first 8 chars of image file name (rrXXX_XX)
-			imgSuffix = image[:-8]
-			# same line print
-			print(imgSuffix, end=', ')
-			# getting the label file for each image
-			labelFile = os.path.join(labelsDir, imgSuffix + ".csv")
+    # Directory containing label CSV files for each .tif image
+    labelsDir = os.path.join(dataDir, "labels(xywh)")
 
-			# finding the label file
-			if os.path.exists(labelFile):	
-				print('Label file exists')
-				objs = []
-				# opening the label file as data
-				with open(labelFile, "r") as data:
-					# reading the data as a DictReader where each entry in the DictReader is a dictionary with keys: x, y, w, h
-					csvReader = csv.DictReader(data)
-					# going through each row in the CSV
-					for row in csvReader:
-						# accessing each field from the CSV
+    dataset_dicts = []  # List to store dictionaries for each image
+    imgID = 1           # ID counter for the images
 
-						# WHAT DO WE DO WITH THESE VALS? ADD TO DICTIONARY?
-						x = float(row['x'])
-						y = float(row['y'])
-						width = float(row['width'])
-						height = float(row['height'])
+    # Directory containing the .tif images
+    imgDir = os.path.join(dataDir, mode)
+    print(f'Loading images from {imgDir} for {mode}')
 
-						# prune spines that were oddly transformed
-						# keep valid bboxes
-						if width * height < 10000:
-							ann = {'bbox': [x, y, width, height],'bbox_mode': BoxMode.XYWH_ABS,'category_id': 0}
-							objs.append(ann)
-				record['annotations'] = objs
-				dataset_dicts.append(record)
-			else:
-				print('Does not have a label file')
-	# a list of dictionaries where each dictionary holds keys: file name, image id, height, width, annotations
-	return dataset_dicts
+    # Iterate over each image in the directory
+    for image in sorted(os.listdir(imgDir)):
+        if image.endswith(".tif"):
+            record = {
+                'file_name': os.path.join(imgDir, image),
+                'image_id': imgID,
+                'height': 1024,
+                'width': 1024
+            }
+            imgID += 1
+
+            # Extracting prefix of the image filename to match with label files
+            imgPrefix = image[:-8]
+            print(imgPrefix, end=', ')
+
+            # Path to the label file for the current image
+            labelFile = os.path.join(labelsDir, imgPrefix + ".csv")
+
+            if os.path.exists(labelFile):
+                print('Label file exists')
+                objs = []
+
+                with open(labelFile, "r") as data:
+                    csvReader = csv.DictReader(data)
+
+                    for row in csvReader:
+                        x, y, width, height = float(row['x']), float(
+                            row['y']), float(row['width']), float(row['height'])
+
+                        # Filter out bounding boxes with areas larger than 10,000
+                        if width * height < 10000:
+                            ann = {
+                                'bbox': [x, y, width, height],
+                                'bbox_mode': BoxMode.XYWH_ABS,
+                                'category_id': 0
+                            }
+                            objs.append(ann)
+                record['annotations'] = objs
+                dataset_dicts.append(record)
+            else:
+                print('Does not have a label file')
+
+    return dataset_dicts
